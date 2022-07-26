d9f290d5c2c208cb061632702b16a931422464a7
Trivender <choudharyrobin2220@gmail.com>
2022-07-26 23:20:00 +0530

    pallidrome

---
+++ Trivender/javaClass/pallidrome.java
@@ -0,0 +1,30 @@
+package javaClass;
+
+import java.util.Scanner;
+
+public class pallidrome {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		String str = sc.next();
+		System.out.println(ispal(str));
+
+	}
+
+	public static boolean ispal(String str) {
+		int i = 0;
+		int j = str.length() - 1;
+		while (i < j) {
+
+			if (str.charAt(i) != str.charAt(j)) {
+				return false;
+			}
+			i++;
+			j--;
+
+		}
+		return true;
+	}
+
+}
