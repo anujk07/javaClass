6b7b22fef66d4e2e6a80cb043b734d1ea32a97c6
Aditya Prakash <adi2000prakash@gmail.com>
2022-08-03 03:18:41 +0530

    new

--- AdityaPrakash/selectionsort.java
+++ AdityaPrakash/selectionsort.java
@@ -1,6 +1,27 @@
 public class selectionsort {
-    public static void main() {
-        
+    public static void main(String args[]) {
+
+        int arr[]={3,4,2,8,1};
+        int small, temp;
+
+        for(int i=0; i<arr.length; i++)
+        {
+            small=i;
+            for(int j=i+1; j<arr.length; j++)
+            {
+                if(arr[small]>arr[j])
+                {
+                    small=j;
+                }
+            }
+            temp=arr[i];
+            arr[i]=arr[small];
+            arr[small]=temp;
+        }
+        for(int i: arr)
+        {
+            System.out.print(i+" ");
+        }
         
     }
     
